46682dc261fd0ce52548c01903b4e073c1abf1e9
Samuel JJ Gosling <samuel.jj.gosling@gmail.com>
2018-04-22 01:00:23 +0100

    Add files via upload

---
+++ DX.sol
@@ -0,0 +1,103 @@
+pragma solidity ^0.4.18;
+
+import "./BasicToken.sol";
+import "./ERC20.sol";
+
+contract Token is ERC20, BasicToken {
+
+  struct Delegate 
+  {
+
+        string user_name;
+        string[] subject_name;
+        uint256 delgation_count;
+        uint256 negvote_count;
+        uint256 posvote_count;
+        uint256 vote_count;
+
+  } 
+ 
+  mapping (address => mapping (address => uint256)) internal allowed;
+  mapping(address => Delegate) public votelog; 
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+    require(_value <= balances[_from]);
+    require(_value <= allowed[_from][msg.sender]);
+
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+ 
+  function allowance(address _owner, address _spender) public view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+
+
+  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+
+  function registerVoter(string user) public {
+
+        Delegate storage z = votelog[msg.sender];
+        require(z.delegation_count == 0);
+        Delegate memory divx = Delegate({user_name: user, subject_name: z.subject_name, delegation_count: 0, vote_count: 0, posvote_count: 0, negvote_count: 0});
+        votelog[msg.sender] = divx; 
+
+  }
+
+
+  function delegationEvent(address voter, uint256 voting_weight, uint256 choice, string project) public dx {
+
+        Delegate storage v = votelog[voter];
+        address[] previous = v.subject_name;
+        string user = v.user_name;
+        uint event = v.delegation_count;
+        uint total = v.vote_count;
+        uint pos = v.posvote_count;
+        uint neg = v.negvote_count;
+        uint option = choice == 0 ? neg : pos;
+        uint a; 
+        uint b;
+
+        voting_weight += option;
+        voting_weight += total;
+        previous.push(project);
+        event++;
+
+        if(option == 0){a = option; b = neg;}
+        else if(option == 1){a = pos; b = option;}
+
+        Delegate memory division = Delegate({user_name: user, subject_name: previous, delegation_count: event, vote_count: total, posvote_count: a, negvote_count: b});
+        votelog[voter] = division;
+
+  }
+
+}
