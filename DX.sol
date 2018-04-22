30e29f40d008e7af39307a82c4dbd242ef2695d4
Samuel JJ Gosling <samuel.jj.gosling@gmail.com>
2018-04-22 04:17:23 +0100

    Update DX.sol

--- DX.sol
+++ DX.sol
@@ -20,51 +20,64 @@ contract Token is ERC20, BasicToken {
   mapping (address => mapping (address => uint256)) internal allowed;
   mapping(address => Delegate) public votelog; 
 
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool)
+  {
+  
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
 
-    balances[_from] = balances[_from].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-    Transfer(_from, _to, _value);
-    return true;
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+        
   }
 
-
-  function approve(address _spender, uint256 _value) public returns (bool) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
+  function approve(address _spender, uint256 _value) public returns (bool)
+  {
+  
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+        
   }
-
  
-  function allowance(address _owner, address _spender) public view returns (uint256) {
-    return allowed[_owner][_spender];
+  function allowance(address _owner, address _spender) public view returns (uint256)
+  {
+  
+        return allowed[_owner][_spender];
+    
+  }
+
+  function increaseApproval(address _spender, uint _addedValue) public returns (bool)
+  {
+  
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    
   }
 
 
-  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
+  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool)
+  {
+  
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+          allowed[msg.sender][_spender] = 0;
+        } else {
+          allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    
   }
 
 
-  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-    uint oldValue = allowed[msg.sender][_spender];
-    if (_subtractedValue > oldValue) {
-      allowed[msg.sender][_spender] = 0;
-    } else {
-      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-    }
-    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
-  }
-
-
-  function registerVoter(string user) public {
+  function registerVoter(string user) public
+  {
 
         Delegate storage x = votelog[msg.sender];
         require(x.delegation_count == 0);
@@ -79,7 +92,8 @@ contract Token is ERC20, BasicToken {
   }
 
 
-  function viewStats(address target) public constant returns(string,string[],uint256,uint256,uint256,uint256) {
+  function viewStats(address target) public constant returns(string,string[],uint256,uint256,uint256,uint256)
+  {
 
         Delegate storage x = votelog[target];
         require(x.username != 0);
@@ -88,7 +102,8 @@ contract Token is ERC20, BasicToken {
   }
 
 
-  function delegationEvent(address voter, uint256 voting_weight, uint256 choice, string project) public {
+  function delegationEvent(address voter, uint256 voting_weight, uint256 choice, string project) public
+  {
 
         uint a; 
         uint b;
