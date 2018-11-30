187744077c69a328a27e5655e2eb406d16b0a629
Samuel JJ Gosling <samuel.jj.gosling@gmail.com>
2018-11-30 22:15:48 +0100

    Update ERC20d.sol

--- ERC20d.sol
+++ ERC20d.sol
@@ -1,264 +1,183 @@
 pragma solidity ^0.4.24;
 
-library SafeMath
-{
+library SafeMath {
 
-  function mul(  uint256 _a,
-                 uint256 _b  ) internal pure returns (uint256 c)
-  {
-
-        if (_a == 0) {
-            return 0;
-            }
-        c = _a * _b;
-        assert(c / _a == _b);
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) { return 0; }
+        uint256 c = a * b;
+        require(c / a == b);
         return c;
+    }
 
-  }
-
-  function div(  uint256 _a,
-                 uint256 _b  ) internal pure returns (uint256)
-  {
-        return _a / _b;
-  }
-
-  function sub(  uint256 _a,
-                 uint256 _b  ) internal pure returns (uint256)
-  {
-
-        assert(_b <= _a);
-        return _a - _b;
-
-  }
-
-  function add(  uint256 _a,
-                 uint256 _b  ) internal pure returns (uint256 c)
-   {
-
-        c = _a + _b;
-        assert(c >= _a);
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
+        uint256 c = a / b;
         return c;
+    }
 
-  }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        uint256 c = a - b;
+        return c;
+    }
 
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a);
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0);
+        return a % b;
+    }
+    
 }
 
+interface IERC20 {
+    
+    function totalSupply() external view returns (uint256);
 
-contract ERC20Basic
-{
+    function balanceOf(address who) external view returns (uint256);
 
-  function totalSupply(
-                           ) public view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
 
-  function balanceOf(  address _who
-                                     ) public view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
 
-  function transfer(  address _to,
-                      uint256 _value  ) public returns (bool);
+    function approve(address spender, uint256 value) external returns (bool);
 
-  event Transfer(  address indexed from,
-                   address indexed to,
-                   uint256 value  );
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    
 }
 
-contract ERC20 is ERC20Basic
-{
+contract ERC20 is IERC20 {
+    
+    using SafeMath for uint256;
 
-  function allowance(  address _owner,
-                       address _spender  ) public view returns (uint256);
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowed;
 
-  function transferFrom(  address _from,
-                          address _to,
-                          uint256 _value  ) public returns (bool);
+    uint256 internal _totalSupply =uint(48070000000).mul(10**uint(18));
+    uint256 private _maxSupply = uint(50600000000).mul(10**uint(18));
+    
+    function totalSupply() public pure returns (uint256 _totalSupply) {}
+    
+    function maxSupply() public pure returns (uint256 _maxSupply) {}
 
-  function approve(  address _spender,
-                     uint256 _value  ) public returns (bool);
+    function balanceOf(address owner) public view returns (uint256) {
+        return _balances[owner];
+    }
 
-  event Approval(  address indexed owner,
-                   address indexed spender,
-                   uint256 value  );
-}
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowed[owner][spender];
+    }
 
-contract BasicToken is ERC20Basic
-{
-
-  using SafeMath for uint256;
-
-  mapping(address => uint256) internal balances;
-
-  uint256 internal totalSupply_;
-
-  function totalSupply(
-                           ) public view returns (uint256)
-  {
-        return totalSupply_;
-  }
-
-  function transfer(  address _to,
-                      uint256 _value  ) public returns (bool)
-  {
-
-        require(_value <= balances[msg.sender]);
-        require(_to != address(0));
-
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        emit Transfer(msg.sender, _to, _value);
+    function transfer(address to, uint256 value) public returns (bool) {
+        _transfer(msg.sender, to, value);
         return true;
+    }
 
-  }
+    function approve(address spender, uint256 value) public returns (bool) {
+        require(spender != address(0));
 
-  function balanceOf(
-                       address _owner  ) public view returns (uint256)
-  {
-        return balances[_owner];
-  }
+        _allowed[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
+        _transfer(from, to, value);
+        emit Approval(from, msg.sender, _allowed[from][msg.sender]);
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        require(spender != address(0));
+
+        _allowed[msg.sender][spender] = _allowed[msg.sender][spender].add(addedValue);
+        emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        require(spender != address(0));
+
+        _allowed[msg.sender][spender] = _allowed[msg.sender][spender].sub(subtractedValue);
+        emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 value) internal {
+        require(to != address(0));
+
+        _balances[from] = _balances[from].sub(value);
+        _balances[to] = _balances[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    function _mint(address account, uint256 value) internal {
+        require(_maxSupply != _totalSupply.add(value));
+        require(account != address(0));
+        
+        _totalSupply = _totalSupply.add(value);
+        _balances[account] = _balances[account].add(value);
+        emit Transfer(address(0), account, value);
+    }
 
 }
 
-contract ERC20d is ERC20, BasicToken
+contract ERC20d is ERC20
 {
 
     bytes32 constant POS = 0x506f736974697665000000000000000000000000000000000000000000000000;
     bytes32 constant NEG = 0x4e65676174697665000000000000000000000000000000000000000000000000;
     bytes32 constant NA = 0x0000000000000000000000000000000000000000000000000000000000000000;
     address public founder = msg.sender;
-    address public admin;
+    address public admin = address(0x0);
 
     modifier only_founder(){ if(msg.sender != founder){revert();} _; }
     modifier only_admin(){ if(msg.sender != admin){revert();} _; }
 
-    mapping(address => mapping (address => uint256)) internal allowed;
     mapping(address => bytes32[25]) public previous;
     mapping(address => bytes32[6]) public delegate;
 
+    uint public totalSupply;
+    uint public decimals;
     string public name;
     string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-
-    constructor(
-                    ) public
+    
+    constructor() public
     {
-
+        _mint(founder, _totalSupply);
+        totalSupply = _totalSupply;
         symbol = "VLDY";
         name = "Validity";
         decimals = 18;
-        totalSupply = uint(50600000000).mul(10**uint(decimals));
-        balances[founder] = totalSupply;
-        emit Transfer(this, founder, totalSupply);
-
     }
 
-    function transferFrom(  address _from,
-                            address _to,
-                            uint256 _value  ) public returns (bool)
+    function adminControl(address _entity) public only_founder { admin = _entity; }
+
+    function registerVoter(bytes32 user) public
     {
-
-        require(_to != address(0));
-        require(_value <= balances[_from]);
-        require(_value <= allowed[_from][msg.sender]);
-
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        emit Transfer(_from, _to, _value);
-        return true;
-
-    }
-
-    function approve(  address _spender,
-                       uint256 _value    ) public returns (bool)
-    {
-
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-
-    }
-
-    function allowance(  address _owner,
-                         address _spender  ) public view returns (uint256)
-    {
-
-        return allowed[_owner][_spender];
-
-    }
-
-    function increaseApproval(  address _spender,
-                                uint _addedValue  ) public returns (bool)
-    {
-
-        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-
-    }
-
-    function decreaseApproval(  address _spender,
-                                uint _subtractedValue  ) public returns (bool)
-    {
-
-        uint oldValue = allowed[msg.sender][_spender];
-        if (_subtractedValue > oldValue) {
-            allowed[msg.sender][_spender] = 0;
-        } else {
-            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-        }
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-
-    }
-
-    function registerVoter(
-                             bytes32 user  ) public
-    {
-
         bytes32[6] storage x = delegate[msg.sender];
         require(x[0] == NA);
         x[0] = user;
-
     }
 
-    function viewStats(
-                         address target  ) public constant returns (  bytes32,
-                                                                      bytes32[25],
-                                                                      uint256,
-                                                                      uint256,
-                                                                      uint256,
-                                                                      uint256,
-                                                                      bytes32  )
+    function viewStats(address target) public view returns (bytes32, bytes32[25] memory, uint256, uint256, uint256, uint256, bytes32)
     {
-
         bytes32[6] storage x = delegate[target];
         bytes32[25] storage y = previous[target];
-
-        return (  x[0],
-                  y,
-                  uint256(x[1]),
-                  uint256(x[2]),
-                  uint256(x[3]),
-                  uint256(x[4]),
-                  x[5]  );
-
+        return (x[0], y, uint(x[1]), uint(x[2]), uint(x[3]), uint(x[4]), x[5]);
     }
 
-    function adminControl(
-                            address entity  ) public only_founder returns (address)
+    function delegationEvent(address voter, uint256 weight, bytes32 choice, bytes32 project) public only_admin
     {
-
-        admin = entity;
-        return admin;
-
-    }
-
-    function delegationEvent(  address voter,
-                               uint256 weight,
-                               bytes32 choice,
-                               bytes32 project  ) public only_admin
-    {
-
         uint256 c = 0;
         bytes32[25] memory prv;
         bytes32[6] storage x = delegate[voter];
@@ -266,13 +185,11 @@ contract ERC20d is ERC20, BasicToken
 
 	    for(uint v = 0; v < y.length ; v++)
 	    {
-
             prv[v] = y[v];
             if(project == y[v]){revert();}
 		    else if(prv[v] == NA){c++;
                                   prv[v] = project;
                                   if(v == y.length-1){c++;} break;}
-
 	    }
 
         require(c > 0);
@@ -286,17 +203,13 @@ contract ERC20d is ERC20, BasicToken
 
         if(c == 1){ x[5] = bytes32("false"); }
         else if(c == 2){ x[5] = bytes32("true"); }
-
     }
 
-    function delegationBonus(
-                               address voter  ) public only_admin
+    function delegationBonus(address voter) public only_admin
     {
-
         bytes32[6] storage x = delegate[voter];
         x[5] = bytes32("false");
         delete previous[voter];
-
     }
 
 }
