9f07771f67dc38d4fd6aede786955ceb04397fcf
Samuel JJ Gosling <samuel.jj.gosling@gmail.com>
2018-12-04 02:35:07 +0100

    Update ERC20d.sol

--- ERC20d.sol
+++ ERC20d.sol
@@ -34,27 +34,7 @@ library SafeMath {
     
 }
 
-interface IERC20 {
-    
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address who) external view returns (uint256);
-
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function transfer(address to, uint256 value) external returns (bool);
-
-    function approve(address spender, uint256 value) external returns (bool);
-
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    
-}
-
-contract ERC20d is IERC20 {
+contract ERC20d {
     
     using SafeMath for uint256;
 
@@ -223,5 +203,9 @@ contract ERC20d is IERC20 {
          vID[_account] = ValidatingIdentifier(_account);
          vAct[_account] = true;
     }
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
 }
